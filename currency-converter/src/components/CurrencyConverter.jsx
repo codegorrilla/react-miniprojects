b318f53e7470a921212478fd686aac6a40c59dad
Sanjib Adhya <codebook@Codebook.local>
2025-01-29 08:48:23 +0530

    fixed currency dropdown issue using Object.keys to convert the object into an array

--- currency-converter/src/components/CurrencyConverter.jsx
+++ currency-converter/src/components/CurrencyConverter.jsx
@@ -13,7 +13,7 @@ const CurrencyConverter = () => {
 		try {
 			const res = await fetch('https://api.frankfurter.app/currencies');
 			const data = await res.json();
-			setCurrencies(data);
+			setCurrencies(Object.keys(data));
 		} catch (error) {
 			console.error('Error fetching', error);
 		}
@@ -36,28 +36,21 @@ const CurrencyConverter = () => {
 			</h2>
 
 			<div>
-				<CurrencyDropdown
-					currencies={currencies}
-					title='From'
-				/>
+				<CurrencyDropdown currencies={currencies} title='From' />
 				{/* swap currency button */}
-				<CurrencyDropdown
-					currencies={currencies}
-					title='To'
-				/>
+				<CurrencyDropdown currencies={currencies} title='To' />
 			</div>
 
 			<div className='mt-4'>
 				<label
 					htmlFor='amount'
-					className='block text-sm font-medium text-gray-700'
-				>
+					className='block text-sm font-medium text-gray-700'>
 					Amount:
 				</label>
 				<input
 					type='number'
 					value={amount}
-					onChange={(e) => setAmount(e.target.value)}
+					onChange={e => setAmount(e.target.value)}
 					className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-indigo-500'
 					name=''
 					id=''
@@ -67,8 +60,7 @@ const CurrencyConverter = () => {
 			<div className='flex justify-end mt-6'>
 				<button
 					onClick={convertCurrency}
-					className='px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-				>
+					className='px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
 					Convert
 				</button>
 			</div>
--- currency-converter/src/components/Dropdown.jsx
+++ currency-converter/src/components/Dropdown.jsx
@@ -1,16 +1,34 @@
-const CurrencyDropdown = ({
+import PropTypes from 'prop-types';
+
+export default function CurrencyDropdown({
 	currencies,
 	currency,
 	setCurrency,
 	favourites,
 	handleFavourite,
-	title,
-}) => {
+	title = '',
+}) {
 	return (
 		<div>
-			<label htmlFor={title}>{title}</label>
+			<label htmlFor={title} className=''>
+				{title}
+			</label>
+			<div>
+				<select value={currency} onChange={e => setCurrency(e.target.value)}>
+					{currencies.map(currency => (
+						<option key={currency} value={currency}></option>
+					))}
+				</select>
+			</div>
 		</div>
 	);
-};
+}
 
-export default CurrencyDropdown;
+CurrencyDropdown.propTypes = {
+	currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+	currency: PropTypes.string.isRequired,
+	setCurrency: PropTypes.func.isRequired,
+	favourites: PropTypes.arrayOf(PropTypes.string),
+	handleFavourite: PropTypes.func,
+	title: PropTypes.string,
+};
