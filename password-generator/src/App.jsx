4b17e5bc002c03f431e668ba48f64b0c808e5ab1
Sanjib Adhya <sanjib@zacksresearch.in>
2025-02-04 08:51:11 -0600

    working on password generator

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,75 @@
+import './App.css';
+import { useState } from 'react';
+import usePasswordGenerator from './hooks/use-password-generator';
+
+function App() {
+	const [length, setLength] = useState(4);
+
+	const [checkboxData, setCheckboxData] = useState([
+		{ title: 'Include Uppercase Letters', state: false },
+		{ title: 'Include Lowercase Letters', state: false },
+		{ title: 'Include Numbers', state: false },
+		{ title: 'Include Symbols', state: false },
+	]);
+
+	const handleCheckboxChange = (i) => {
+		const updatedChckboxData = [...checkboxData];
+		updatedChckboxData[i].state = !updatedChckboxData[i].state;
+
+		setCheckboxData(updatedChckboxData);
+	};
+
+	const { password, errorMessage, generatedPassword } = usePasswordGenerator();
+
+	return (
+		<div className='container'>
+			{/* Password text and copy */}
+			{password && (
+				<div className='header'>
+					<div className='title'>{password}</div>
+					<button
+						className='copyBtn'
+						onClick={() => {}}
+					>
+						Copy
+					</button>
+				</div>
+			)}
+			{/* Character length */}
+			<div className='charlength'>
+				<span>
+					<label>Character length</label>
+					<label>{length}</label>
+				</span>
+				<input
+					type='range'
+					min='4'
+					max='20'
+					value={length}
+					onChange={(e) => setLength(e.target.value)}
+				/>
+			</div>
+
+			{/* Checkboxes*/}
+			<div className='checkboxes'>
+				{checkboxData.map((checkbox, index) => {
+					return (
+						<div key={index}>
+							<input
+								type='checkbox'
+								checked={checkbox.state}
+								onChange={() => handleCheckboxChange(index)}
+							/>
+							<label>{checkbox.title}</label>
+						</div>
+					);
+				})}
+			</div>
+			{/* Strength */}
+			{/* Generate button */}
+			<button className='generateBtn'>Generate Password</button>
+		</div>
+	);
+}
+
+export default App;
